cb4de94217f67fa9c4fc27619293407b06c050df
Carlos Daniel <carlosporrasayala@gmail.com>
2024-05-13 10:58:45 -0500

    Generate unique ids with the uuid library

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,6 @@
 import { useState, ChangeEvent, FormEvent, Dispatch } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import { categories } from '../data/categories';
 import type { Activity } from '../types';
 import { ActivityActions } from '../reducers/activity-reducer';
@@ -7,7 +9,12 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>;
 };
 
-const initialState = { category: 1, name: '', calories: 0 };
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: '',
+  calories: 0,
+};
 
 export default function Form({ dispatch }: FormProps) {
   const [activity, setActivity] = useState<Activity>(initialState);
@@ -31,7 +38,7 @@ export default function Form({ dispatch }: FormProps) {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch({ type: 'save-activity', payload: { newActivity: activity } });
-    setActivity(initialState);
+    setActivity({ ...initialState, id: uuidv4() });
   };
 
   return (
